ed15eda1371a0ed741468446b2c612c8a01d16f4
Maciej Maćkowiak <maciej.mackowiak@gmail.com>
2022-05-27 20:43:54 +0200

    bump version

--- wp-api-swaggerui.php
+++ wp-api-swaggerui.php
@@ -10,7 +10,7 @@
  * @wordpress-plugin
  * Plugin Name: WP API SwaggerUI
  * Description: WordPress REST API with Swagger UI.
- * Version:     1.1.2.1
+ * Version:     1.1.2.2
  * Author:      Agus Suroyo, APKMirror.com
  * License:     GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
